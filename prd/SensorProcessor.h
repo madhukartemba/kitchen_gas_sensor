e3dfa396d4d4836d19a4a812c2a541128e594fd3
madhukartemba <madhusmiles.madhukar@gmail.com>
2026-04-11 23:08:59 +0530

    add sensor processor file

---
+++ prd/SensorProcessor.h
@@ -0,0 +1,96 @@
+#pragma once
+#include <vector>
+
+enum SensorState {
+  WARMUP,
+  NORMAL,
+  ALARM,
+  COOLDOWN,
+  ERROR,
+};
+
+class SensorProcessor {
+private:
+  double readingSum;
+  std::vector<double> readings;
+  int numberOfReadings;
+  unsigned long warmupTime;
+  unsigned long cooldownTime;
+  SensorState state;
+
+  // Thresholds
+  double absoluteThreshold;
+  double relativeThreshold;
+
+  // Timers
+  unsigned long cooldownTimer;
+
+  void setState(SensorState newState, unsigned long timestamp) {
+    switch (newState) {
+    case WARMUP:
+      state = newState;
+      break;
+    case NORMAL:
+      if (state != ALARM &&
+          (state != COOLDOWN || timestamp - cooldownTimer > cooldownTime)) {
+        state = newState;
+      }
+      break;
+    case ALARM:
+      if (state != COOLDOWN) {
+        state = newState;
+      } else if (timestamp - cooldownTimer > cooldownTime) {
+        state = newState;
+      }
+      break;
+    case COOLDOWN:
+      if (state != COOLDOWN) {
+        state = newState;
+        cooldownTimer = timestamp;
+      }
+      break;
+    case ERROR:
+      state = newState;
+      break;
+    };
+  }
+
+public:
+  SensorProcessor(unsigned long warmupTime, unsigned long cooldownTime,
+                  int numberOfReadings)
+      : warmupTime(warmupTime), cooldownTime(cooldownTime),
+        numberOfReadings(numberOfReadings) {
+    this->readingSum = 0;
+    setState(WARMUP, 0);
+    this->cooldownTimer = 0;
+  }
+
+  void update(double reading, unsigned long timestamp) {
+    if (state == WARMUP) {
+      if (timestamp > warmupTime) {
+        setState(NORMAL, timestamp);
+      }
+    } else {
+      // Do the normal processing everytime
+      readings.push_back(reading);
+      if (readings.size() > numberOfReadings) {
+        readings.erase(readings.begin());
+      }
+
+      double readingDiff = reading - readings.front();
+
+      if (readings.size() >= numberOfReadings &&
+          readingDiff > relativeThreshold) {
+        setState(ALARM, timestamp);
+      } else if (reading > absoluteThreshold) {
+        setState(ALARM, timestamp);
+      } else {
+        setState(NORMAL, timestamp);
+      }
+    }
+  }
+
+  SensorState getState() { return state; }
+
+  void resetAlarm(unsigned long timestamp) { setState(COOLDOWN, timestamp); }
+};
